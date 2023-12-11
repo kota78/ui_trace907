f83b5665882b28dbd0b4dca607400b7e6bc50efd
ko_cha78 <kota1725@gmail.com>
2023-12-11 22:13:00 +0900

    2023/12/11 定例会にて同様の題材が出たため少し修正

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 String title = "Stay With Me";
 String artist = "Sam Smith";
 
-
 void main() {
   runApp(MyApp());
 }
@@ -25,160 +24,215 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-
   final String title;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-
-
-
 class _MyHomePageState extends State<MyHomePage> {
-
   int height = 180;
   bool _play = false;
   bool _favorite = false;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(children: [
-        //Positioned.fill(child: Image.asset('assets/', fit: BoxFit.fill,),),
-        Positioned.fill(child: Container(color: Colors.black,),),
-        Column(children: [
-          SizedBox(height: 30,),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Stack(
             children: [
+              //Positioned.fill(child: Image.asset('assets/', fit: BoxFit.fill,),),
+              Positioned.fill(
+                child: Container(
+                  color: Colors.black87,
+                ),
+              ),
               Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: InkWell(onTap: (){}, child: Icon(Icons.arrow_downward, color: Colors.white, size: 40,)),
-              ),
-              Text("Sam Smith", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: InkWell(onTap: (){}, child: Icon(Icons.cancel, color: Colors.white, size: 30,)),
+                padding: const EdgeInsets.all(24.0),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        InkWell(
+                          onTap: () {},
+                          child: Icon(
+                            Icons.expand_more,
+                            color: Colors.white,
+                            size: 40,
+                          ),
+                        ),
+                        Text(
+                          "Sam Smith",
+                          style: TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.bold),
+                        ),
+                        InkWell(
+                          onTap: () {},
+                          child: Icon(
+                            Icons.more_horiz,
+                            color: Colors.white,
+                            size: 30,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 48,
+                    ),
+                    Image.asset(
+                      "assets/picture.jpg",
+                      fit: BoxFit.cover,
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(4.0),
+                              child: Text(
+                                title,
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(4.0),
+                              child: Text(
+                                artist,
+                                style: TextStyle(color: Colors.grey, fontSize: 15),
+                              ),
+                            ),
+                          ],
+                        ),
+                        (() {
+                          if (_favorite == false) {
+                            return InkWell(
+                                highlightColor: Colors.white,
+                                splashColor: Colors.white,
+                                onTap: () {
+                                  setState(() {
+                                    _favorite = true;
+                                  });
+                                },
+                                child: Icon(
+                                  Icons.favorite_border,
+                                  color: Colors.white,
+                                  size: 30,
+                                ));
+                          } else {
+                            return InkWell(
+                                highlightColor: Colors.white,
+                                splashColor: Colors.white,
+                                onTap: () {
+                                  setState(() {
+                                    _favorite = false;
+                                  });
+                                },
+                                child: Icon(
+                                  Icons.favorite,
+                                  color: Colors.white,
+                                  size: 30,
+                                ));
+                          }
+                        })(),
+                      ],
+                    ),
+                    SizedBox(height: 12),
+                    Slider(
+                      value: height.toDouble(),
+                      min: 120,
+                      max: 220,
+                      onChanged: (double newValue) {
+                        setState(() {
+                          height = newValue.round();
+                        });
+                      },
+                    ),
+                    SizedBox(height: 12),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        InkWell(
+                            child: Icon(
+                          Icons.shuffle,
+                          color: Colors.white,
+                        )),
+                        Icon(
+                          Icons.skip_previous,
+                          color: Colors.white,
+                          size: 50,
+                        ),
+                        (() {
+                          if (_play == false) {
+                            return InkWell(
+                                highlightColor: Colors.white,
+                                splashColor: Colors.white,
+                                onTap: () {
+                                  setState(() {
+                                    _play = true;
+                                  });
+                                },
+                                child: Icon(
+                                  Icons.play_arrow_outlined,
+                                  color: Colors.white,
+                                  size: 70,
+                                ));
+                          } else {
+                            return InkWell(
+                                highlightColor: Colors.white,
+                                splashColor: Colors.white,
+                                onTap: () {
+                                  setState(() {
+                                    _play = false;
+                                  });
+                                },
+                                child: Icon(
+                                  Icons.stop,
+                                  color: Colors.white,
+                                  size: 70,
+                                ));
+                          }
+                        })(),
+                        Icon(
+                          Icons.skip_next,
+                          color: Colors.white,
+                          size: 50,
+                        ),
+                        Icon(
+                          Icons.repeat,
+                          color: Colors.white,
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Icon(
+                          Icons.speaker_group,
+                          color: Colors.white,
+                        ),
+                        Icon(
+                          Icons.list,
+                          color: Colors.white,
+                        )
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
-          Expanded(child: Container()),
-          Expanded(flex: 5,child: Container(child: Image.asset("assets/picture.jpg"))),
-          SizedBox(height: 20,),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: Text(title, style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: Text(artist, style: TextStyle(color: Colors.grey, fontSize: 15),),
-                  ),
-                ],
-              ),
-              (() {
-                if (_favorite == false) {
-                  return InkWell(
-                      highlightColor: Colors.white,
-                      splashColor: Colors.white,
-                      onTap: (){
-                        setState(() {
-                          _favorite = true;
-                        });
-                      },
-                      child: Icon(
-                        Icons.favorite_border,
-                        color: Colors.white,
-                        size: 30,
-                      )
-                  );
-                } else {
-                  return InkWell(
-                      highlightColor: Colors.white,
-                      splashColor: Colors.white,
-                      onTap: (){
-                        setState(() {
-                          _favorite = false;
-                        });
-                      },
-                      child: Icon(
-                        Icons.favorite,
-                        color: Colors.white,
-                        size: 30,
-                      )
-                  );
-                }
-              })(),
-            ],
-          ),
-          Expanded(child: Container()),
-          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              InkWell(child: Icon(Icons.cancel, color: Colors.white,)),
-              Icon(Icons.arrow_left, color: Colors.white, size: 50,),
-              (() {
-                if (_play == false) {
-                  return InkWell(
-                      highlightColor: Colors.white,
-                      splashColor: Colors.white,
-                      onTap: (){
-                        setState(() {
-                          _play = true;
-                        });
-                      },
-                      child: Icon(
-                        Icons.play_arrow_outlined,
-                        color: Colors.white,
-                        size: 70,
-                      )
-                  );
-                } else {
-                  return InkWell(
-                      highlightColor: Colors.white,
-                      splashColor: Colors.white,
-                      onTap: (){
-                        setState(() {
-                          _play = false;
-                        });
-                      },
-                      child: Icon(
-                        Icons.stop,
-                        color: Colors.white,
-                        size: 70,
-                      )
-                  );
-                }
-              })(),
-              Icon(Icons.arrow_right, color: Colors.white, size: 50,),
-              Icon(Icons.camera, color: Colors.white,),
-            ],
-          ),
-          Expanded(child: Container()),
-          Slider(
-            value: height.toDouble(),
-            min: 120,
-            max: 220,
-            onChanged: (double newValue) {
-              setState(() {
-                height = newValue.round();
-              });
-            },
-          ),
-          Expanded(child: Container()),
-          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Icon(Icons.hardware, color: Colors.white,),
-              Icon(Icons.list, color: Colors.white,)
-            ],),
-          Expanded(child: Container()),
-        ],),
-      ],),
+        ),
+      ),
     );
   }
 }
